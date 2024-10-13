3b20f53f91834a0f85e4a11f6336ee2ff319dd0f
gerasaka <gerryjulioo@gmail.com>
2024-10-13 19:28:42 +0700

    chore: change srcDir to `app`

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const x = [1, 2, 3, 4, 5, 6];
+</script>
+
+<template>
+  <div>
+    <span>hello you</span>
+          <h1>wow i can be formatted</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  srcDir: "app/",
+});
