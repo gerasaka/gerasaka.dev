d255ab81e9273fb4e22af045077b3e94602e843f
gerasaka <gerryjulioo@gmail.com>
2024-10-19 06:57:03 +0700

    chore: implement nuxt 4 folder structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,28 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    // normalizeComponentNames: false,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
   devtools: { enabled: true },
-  srcDir: "app/",
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxtjs/tailwindcss"],
   tailwindcss: {
     cssPath: ["~/assets/styles/tailwind.css", { injectPosition: "first" }],
