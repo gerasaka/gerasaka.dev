48ad835d2561158c6f2fb6cada6a1f90e143ca1f
gerasaka <gerryjulioo@gmail.com>
2024-10-19 14:54:44 +0700

    refactor: change icon components to `nuxt/icon`

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,24 @@
+<script lang="ts" setup>
+  import { useFavicon } from '@vueuse/core';
+
+  useFavicon().value = '/favicon-light.ico';
+</script>
+
 <template>
-  <div>
-    <IconLogo />
+  <div class="relative">
+    <NuxtImg src="/lg.png" class="absolute top-0 right-0 -z-10" role="presentation" />
 
-    <h1>Gerry Julio</h1>
-    <p>Frontend Engineer</p>
+    <Icon name="grsk:logo" />
 
-    <div>
-      <IconLinkedin />
-      <IconGithub />
-      <IconX />
-      <IconLeetcode />
-      <IconMail />
+    <h1 class="font-dark font-serif font-extrabold text-6xl">Gerry Julio</h1>
+    <p class="font-light text-2xl">Frontend Engineer</p>
+
+    <div class="flex gap-4">
+      <Icon name="ph:envelope-fill" />
+      <Icon name="simple-icons:leetcode" />
+      <Icon name="simple-icons:linkedin" />
+      <Icon name="simple-icons:github" />
+      <Icon name="simple-icons:x" />
     </div>
   </div>
 </template>
