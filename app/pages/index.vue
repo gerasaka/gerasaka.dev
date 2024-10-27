d1cfc4196a7566a7bfb5d858e18bc5ca50efb731
gerasaka <gerryjulioo@gmail.com>
2024-10-27 10:10:54 +0700

    feat: add profile description

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-  <div class="relative bg-base h-svh">
+  <div class="bg-background h-svh">
     <img
       srcset="
         ~/assets/images/sm.png  640w,
@@ -16,48 +16,67 @@
       sizes="(max-width: 640px) 100vw, (max-width: 1023px) 800px, 1000px"
       src="~/assets/images/sm.png"
       role="presentation"
-      class="absolute top-0 right-0 z-10"
+      class="absolute top-0 right-0"
     />
 
     <div
-      class="flex flex-col justify-center h-5/6 mx-auto px-8 md:px-20 lg:px-32 lg:max-w-5xl relative z-20"
+      class="flex flex-col justify-center h-5/6 mx-auto px-8 md:px-20 lg:px-32 lg:max-w-5xl relative"
     >
-      <Icon name="grsk:logo" class="w-20 h-20" />
+      <Icon name="grsk:logo" class="w-16 h-16" />
 
       <div>
         <h1 class="heading">Gerry Julio</h1>
-        <p class="text-light text-2xl mt-2">Frontend Engineer</p>
+        <p class="text-light font-light text-lg md:text-xl mt-2">Frontend Engineer</p>
+
+        <p class="text-dark text-base mt-10">
+          I craft user-centric web experiences where form follows function, and focus on delivering
+          innovative solutions.
+        </p>
+
+        <p class="text-dark text-base mt-4">
+          Currently at
+          <NuxtLink
+            to="https://www.doku.com/en-US"
+            target="_blank"
+            class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-dark to-[#961912] transition-colors duration-500 hover:text-[#E1251B]"
+          >
+            DOKU
+          </NuxtLink>
+          , one of Indonesia's leading payment gateway companies, I build high-performance
+          applications that transform payment processes into seamless user experiences, prioritizing
+          security and maintainability.
+        </p>
       </div>
 
       <div class="flex items-center gap-4 mt-8 text-light">
         <NuxtLink to="mailto:gerryjulioo@gmail.com">
           <Icon
             name="ph:envelope-fill"
-            class="transition-colors duration-300 w-7 h-7 hover:text-dark"
+            class="transition-colors duration-300 w-6 h-6 md:w-7 md:h-7 hover:text-dark"
           />
         </NuxtLink>
         <NuxtLink to="https://www.linkedin.com/in/gerryjulio/">
           <Icon
             name="simple-icons:linkedin"
-            class="transition-colors duration-300 w-6 h-6 hover:text-dark"
+            class="transition-colors duration-300 w-5 h-5 md:w-6 md:h-6 hover:text-dark"
           />
         </NuxtLink>
         <NuxtLink to="https://github.com/gerasaka/">
           <Icon
             name="simple-icons:github"
-            class="transition-colors duration-300 w-6 h-6 hover:text-dark"
-          />
-        </NuxtLink>
-        <NuxtLink to="https://leetcode.com/u/gerasaka/">
-          <Icon
-            name="simple-icons:leetcode"
-            class="transition-colors duration-300 w-6 h-6 hover:text-dark"
+            class="transition-colors duration-300 w-5 h-5 md:w-6 md:h-6 hover:text-dark"
           />
         </NuxtLink>
         <NuxtLink to="https://x.com/gerasaka/">
           <Icon
             name="simple-icons:x"
-            class="transition-colors duration-300 w-6 h-6 hover:text-dark"
+            class="transition-colors duration-300 w-5 h-5 md:w-6 md:h-6 hover:text-dark"
+          />
+        </NuxtLink>
+        <NuxtLink to="https://leetcode.com/u/gerasaka/">
+          <Icon
+            name="simple-icons:leetcode"
+            class="transition-colors duration-300 w-5 h-5 md:w-6 md:h-6 hover:text-dark"
           />
         </NuxtLink>
       </div>
@@ -67,6 +86,6 @@
 
 <style lang="scss">
   .heading {
-    @apply font-serif font-extrabold text-6xl leading-[5rem] inline-block mt-12 md:mt-20 text-transparent bg-clip-text bg-gradient-to-r from-dark from-30% via-primary-800 via-60% to-primary-500;
+    @apply font-serif font-extrabold text-5xl leading-[4rem] inline-block mt-12 md:mt-20 text-transparent bg-clip-text bg-gradient-to-r from-dark from-30% via-primary-800 via-60% to-primary-500;
   }
 </style>
