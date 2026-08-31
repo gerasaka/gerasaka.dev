8c2abab16198a67fad831512f6bf17a47373a94f
gerasaka <gerryjulioo@gmail.com>
2026-08-31 19:36:41 +0700

    feat: extract fade-in animation to dedicated fade component

---
+++ app/components/FadeIn.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  import { provide } from 'vue';
+
+  const { step = 70, base = 0 } = defineProps<{ step?: number; base?: number }>();
+
+  let index = 0;
+
+  provide(FadeInKey, {
+    next: () => base + index++ * step,
+  });
+</script>
+
+<template>
+  <slot />
+</template>
